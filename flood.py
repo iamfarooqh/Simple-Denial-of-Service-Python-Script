b3120409af41523d056d27ad3655e0a11047109f
Farooq Mohammad <iamfarooqh@users.noreply.github.com>
2017-10-27 03:11:56 +0530

    Made major changes to the script.

--- flood.py
+++ flood.py
@@ -1,26 +1,49 @@
 #!/usr/bin/env python
 
 # Python script to automate Flooding tests.
-# Written by Farooq Mohammad 
-# Use the script at your own risk
+# Written by Farooq Mohammad
+# Use the script at your own risk. There is lot much validation that needs to be done here.
 
 import sys
 import os
-import subprocess
-os.system('clear')   
-#ip = raw_input("Please provide the IP address of the Test Server: ")
-ip = sys.argv[1]
-print("")
+
+os.system('clear')
+
+ip = raw_input("Please provide the IP address of the Test Server: ")
+#ip = sys.argv[1]
+
 print("")
 print ("++++++++++Starting TCP Protocol Scan +++++++++++++")
 tcpScan = os.system("/usr/bin/nmap -sS -p 1-100 -Pn --webxml -oA TCPSCAN %s" %ip)
-#print tcpScan
 print ("++++++++++ END OF TCP Protocol Scan ++++++++++++++")
 #print("")
-#print("")
-ports = os.popen("/bin/cat  $PWD/TCPSCAN.xml | grep 'portid' |cut -d '=' -f 3 | cut -d '>' -f 1 | cut -d '\"' -f 2 ").read()
-print ports
-for port in ports.split():
-    #flood = os.system("/bin/ping  -c %s " %port)
-    print "hello"
-    print port
+
+openPorts = os.popen("/bin/cat  $PWD/TCPSCAN.xml | grep 'portid' |cut -d '=' -f 3 | cut -d '>' -f 1 | cut -d '\"' -f 2 ").read()
+print ("The list of open ports are: "),
+print openPorts.split()
+
+for port in openPorts.split():
+    flood = os.system("/bin/ping %s -c %s " %(ip,port))
+    #print port
+
+print ("")
+print "End of Flooding attack. "
+print "Now lets do a Fresh nmap scan again and compare the results"
+
+print("")
+print ("++++++++++Starting TCP Protocol Scan +++++++++++++")
+newTcpScan = os.system("/usr/bin/nmap -sS -p 1-100 -Pn --webxml -oA TCPSCANnew %s" %ip)
+print ("++++++++++ END OF TCP Protocol Scan ++++++++++++++")
+
+newPorts = os.popen("/bin/cat  $PWD/TCPSCANnew.xml | grep 'portid' |cut -d '=' -f 3 | cut -d '>' -f 1 | cut -d '\"' -f 2 ").read()
+print ("")
+
+print ("The list of open ports after flood are: "),
+print newPorts.split()
+print ("The list of open ports before flood are: "),
+print openPorts.split()
+
+if openPorts.split() == newPorts.split():
+        print "Flooding Test is passed. All ports seems to be OPEN after the tests"
+else:
+        print "Some ports are not open. Please verify if the ports are reachable"
